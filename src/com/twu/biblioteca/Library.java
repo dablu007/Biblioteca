b8c3d65071bb837808ec6d124f55980f6aa7232b
Dablu Kumar <dabluk@thoughtworks.com>
2015-07-28 13:30:09 +0530

    Library can list all the books inside

--- src/com/twu/biblioteca/Library.java
+++ src/com/twu/biblioteca/Library.java
@@ -1,4 +1,17 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+
 public class Library {
+    private ArrayList<Book> books;
+
+    public Library(ArrayList<Book> books) {
+        this.books = books;
+    }
+
+    public void showListOfBooks() {
+        for(Book book: books){
+            book.show();
+        }
+    }
 }
--- test/com/twu/biblioteca/LibraryTest.java
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -1,4 +1,23 @@
 package com.twu.biblioteca;
 
+import org.junit.Test;
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+
 public class LibraryTest {
+    @Test
+    public void shouldAddABookAndShowListOfBooksInTheLibrary() {
+        Book virtualBook = mock(Book.class);
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(virtualBook);
+        Library library = new Library(books);
+
+        library.showListOfBooks();
+
+        verify(virtualBook, atLeast(1)).show();
+    }
 }
