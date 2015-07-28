e575aeedabb2b2e465000ac86becc449b80befe4
Dablu Kumar <dabluk@thoughtworks.com>
2015-07-29 00:09:32 +0530

    Application is running for the given set of list books option

--- src/com/twu/biblioteca/Main.java
+++ src/com/twu/biblioteca/Main.java
@@ -1,16 +1,29 @@
 package com.twu.biblioteca;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         View view = new View();
         Inputs inputs = new Inputs();
         ArrayList<HashMap> books = inputs.addBooks();
         Library library = new Library(books);
         view.showWelcomeMessage();
-        view.showListBookMessage();
-        library.showListOfBooks(view);
+        view.showOptions();
+        try {
+            System.out.println("Enter Your Choice");
+            int option = Integer.parseInt(br.readLine());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Menu menu = new Menu(library, view);
+        menu.executeCommand(1);
     }
 }
