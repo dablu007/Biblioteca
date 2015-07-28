31832efbd9fe97b0cf48420265d6275b0365906b
Dablu Kumar <dabluk@thoughtworks.com>
2015-07-28 11:17:30 +0530

    Welcome message added to the application

---
+++ test/com/twu/biblioteca/MessagesTest.java
@@ -0,0 +1,32 @@
+package com.twu.biblioteca;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class MessagesTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
+    @Test
+    public void shouldCheckForTheWelcomeMessage() {
+        Messages messages = new Messages();
+
+        messages.showMessage();
+        assertEquals("Welcome to The Bangalore Public Library\n",outContent.toString());
+    }
+}
