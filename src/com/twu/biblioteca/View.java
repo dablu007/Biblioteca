75bfbc5ae3d0a480a0313ba0c2598e489ba4a1cd
Dablu Kumar <dabluk@thoughtworks.com>
2015-07-28 16:06:46 +0530

    View is modified and output is structured in columns

--- src/com/twu/biblioteca/View.java
+++ src/com/twu/biblioteca/View.java
@@ -6,9 +6,10 @@ public class View {
     }
 
     public void showBook(Book book) {
-        System.out.println("Book Name " + book.getBookNo());
-        System.out.println("Book No " + book.getBookName());
-        System.out.println("Author " + book.getAuthor());
-        System.out.println("Publication " + book.getPublication() + "\n");
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+        System.out.println("------------------------------Avilable Books--------------------------");
+        System.out.println(String.format("%s%17s%17s%27s","Book No.","BookName","Author","Publication"));
+        System.out.println(book.getBookNo() + book.getBookName() + book.getAuthor() + book.getPublication());
     }
 }
