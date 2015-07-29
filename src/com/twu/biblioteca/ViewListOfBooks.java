d43a78439bdb047a3ead5871a19d12de3f8c59cb
Dablu Kumar <dabluk@thoughtworks.com>
2015-07-29 20:46:07 +0530

    [Dablu] View for listing of books added

--- src/com/twu/biblioteca/ViewListOfBooks.java
+++ src/com/twu/biblioteca/ViewListOfBooks.java
@@ -1,4 +1,25 @@
 package com.twu.biblioteca;
 
-public class ViewListOfBooks {
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ViewListOfBooks implements IView{
+    private Formatters formatters;
+    public ViewListOfBooks(Formatters formatters) {
+        this.formatters = formatters;
+    }
+
+    @Override
+    public void show() {
+        System.out.println("------------------------------Avilable Books--------------------------");
+        System.out.println(String.format("%s%17s%17s%27s", "Book No.", "BookName", "Author", "Publication"));
+        ArrayList<HashMap> books = formatters.format();
+        for (int i = 0; i < books.size(); i++) {
+            HashMap book = books.get(i);
+            System.out.println(String.format("%s%20s%27s%17s", book.get("bookNo"), book.get("bookName"), book.get("author"),
+                    book.get("publication")));
+            System.out.println("----------------------------------------------------------------------");
+        }
+
+    }
 }
--- test/com/twu/biblioteca/ViewListOfBooksTest.java
+++ test/com/twu/biblioteca/ViewListOfBooksTest.java
@@ -1,4 +1,45 @@
 package com.twu.biblioteca;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class ViewListOfBooksTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Test
+    public void shouldListAllTheBooksAvailableInLibrary() {
+        HashMap book = new HashMap();
+        book.put("bookNo", 1);
+        book.put("bookName", "Java");
+        book.put("availability", "available");
+        book.put("author", "James Gosling");
+        book.put("publication", "TMH");
+        ArrayList<HashMap> books = new ArrayList<HashMap>();
+        books.add(book);
+        Library library = new Library(books);
+        Formatters formatters = new Formatters(library);
+        ViewListOfBooks viewListOfBooks = new ViewListOfBooks(formatters);
+        viewListOfBooks.show();
+
+        String expectedList = "------------------------------Avilable Books--------------------------\n"
+                +String.format("%s%17s%17s%27s", "Book No.", "BookName", "Author", "Publication") + "\n" +
+                String.format("%s%20s%27s%17s", book.get("bookNo"), book.get("bookName"), book.get("author"),
+                        book.get("publication")) + "\n" +
+                        "----------------------------------------------------------------------\n";
+        assertEquals(expectedList.toString(), outContent.toString());
+    }
 }
