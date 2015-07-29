ebd6b4331fec79af3db34d615c7c4ade4faedddb
Dablu Kumar <dabluk@thoughtworks.com>
2015-07-29 18:03:14 +0530

    [Dablu] Formatter can format the list of books

---
+++ src/com/twu/biblioteca/Formatters.java
@@ -0,0 +1,32 @@
+package com.twu.biblioteca;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Formatters {
+    private Library library;
+    private HashMap map;
+    public Formatters(Library library) {
+        this.library = library;
+    }
+
+    public ArrayList<HashMap> format() {
+        map = new HashMap();
+        String value = library.toString();
+        map = new HashMap<>();
+        String[] bookValueAfterSplit = value.split("::");
+        System.out.println(bookValueAfterSplit[0]);
+        ArrayList<HashMap> books = new ArrayList<>();
+        for (String s : bookValueAfterSplit) {
+            String[] book = s.split(":");
+            map = new HashMap();
+            map.put("author",book[0]);
+            map.put("bookNo",book[1]);
+            map.put("bookName",book[4]);
+            map.put("availability",book[3]);
+            map.put("publication",book[2]);
+            books.add(map);
+        }
+        return books;
+    }
+}
---
+++ test/com/twu/biblioteca/FormattersTest.java
@@ -0,0 +1,39 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+
+public class FormattersTest {
+    private HashMap book;
+    private ArrayList<HashMap> books;
+    private Library library;
+    private HashMap book1;
+
+    @Test
+    public void shouldListBooksAvailableFromLibrary() {
+        book = new HashMap();
+        book1 = new HashMap();
+        book.put("bookNo", 1);
+        book.put("bookName", "Java");
+        book.put("availability", "available");
+        book.put("author", "James Gosling");
+        book.put("publication", "TMH");
+        book1.put("bookNo", 2);
+        book1.put("bookName", "C++");
+        book1.put("availability", "available");
+        book1.put("author", "James Gosling");
+        book1.put("publication", "TMH");
+        books = new ArrayList<HashMap>();
+        books.add(book);
+        books.add(book1);
+        library = new Library(books);
+        Formatters formatters = new Formatters(library);
+        assertEquals(books.toString(), formatters.format().toString());
+    }
+}
