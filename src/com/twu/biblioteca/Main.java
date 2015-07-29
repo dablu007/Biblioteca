77f0a50cff1c4a2cdbe47d7d4a9ad85a6c45bf4b
Dablu Kumar <dabluk@thoughtworks.com>
2015-07-29 21:07:36 +0530

    [Dablu] List books working in main application with the new View

--- src/com/twu/biblioteca/Main.java
+++ src/com/twu/biblioteca/Main.java
@@ -9,19 +9,21 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         View view = new View();
+        IView welcomeView = new WelcomeMessage();
         Inputs inputs = new Inputs();
         ArrayList<HashMap> books = inputs.addBooks();
-        Library library = new Library(books, view);
-        view.showWelcomeMessage();
+        Library library = new Library(books);
+        Formatters formatters = new Formatters(library);
+        IView listBooksView = new ViewListOfBooks(formatters);
+        welcomeView.show();
         int option;
         while (true){
             try {
                 view.showOptions();
                 System.out.println("Enter Your Choice");
                 option = Integer.parseInt(br.readLine());
-                Menu menu = new Menu(library, view);
+                Menu menu = new Menu(library, listBooksView);
                 menu.executeCommand(option);
 
             } catch (IOException e) {
--- src/com/twu/biblioteca/Menu.java
+++ src/com/twu/biblioteca/Menu.java
@@ -5,14 +5,24 @@ public class Menu {
     private Library library;
     private View view;
     private Inputs inputs;
-    public Menu(Library library,View view) {
+    private IView viewBookList;
+
+    public Menu(Library library,IView view) {
         this.library = library;
-        this.view = view;
+        this.viewBookList = view;
+    }
+
+    public Menu(IView view) {
+        viewBookList = view;
+    }
+
+    public Menu(Library library, View view) {
+
     }
 
     public void executeCommand(int command) {
         if (command == 1){
-            this.library.showListOfBooks();
+            viewBookList.show();
         }
         else if (command == 2){
             int bookno = new Inputs().getBookNoForCheckout();
