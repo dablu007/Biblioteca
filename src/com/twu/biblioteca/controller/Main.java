31143a748591246fedbef500ea5a9907a7ae7bf6
Dablu Kumar <dabluk@thoughtworks.com>
2015-08-01 12:16:35 +0530

    [Dablu] whole menu removed from the main application and can get operation from Menu for execution

--- src/com/twu/biblioteca/controller/Main.java
+++ src/com/twu/biblioteca/controller/Main.java
@@ -1,10 +1,13 @@
 package com.twu.biblioteca.controller;
 
-import com.twu.biblioteca.model.*;
-import com.twu.biblioteca.view.*;
+import com.twu.biblioteca.model.Book;
+import com.twu.biblioteca.model.Inputs;
+import com.twu.biblioteca.model.Library;
+import com.twu.biblioteca.view.IView;
+import com.twu.biblioteca.view.ViewShowOptions;
+import com.twu.biblioteca.view.ViewWelcomeMessage;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
@@ -15,35 +18,16 @@ public class Main {
         Inputs inputs = new Inputs();
         ArrayList<Book> books = inputs.addBooks();
         Library library = new Library(books, new ArrayList<Book>());
-        IView viewListOfBooks = new ViewListOfBooks(library);
         IView optionsView = new ViewShowOptions();
+        MenuOptions menuOptions = new MenuOptions(library);
         welcomeView.show();
         String  option;
-
-        ListBooks listBooks = new ListBooks(library, viewListOfBooks);
-        CheckoutBook checkoutBook = new CheckoutBook(library);
-        ReturnBook returnBook = new ReturnBook(library);
-        Exit exit = new Exit(library);
-        InvalidMessage invalidMessage = new InvalidMessage(new ViewInvalidMessage());
-        HashMap menuOptions = new HashMap();
-        menuOptions.put("1", listBooks);
-        menuOptions.put("2", checkoutBook);
-        menuOptions.put("3", returnBook);
-        menuOptions.put("4", exit);
-        menuOptions.put("5", invalidMessage);
         while (true){
             optionsView.show();
             System.out.println("Enter Your Choice");
             option = scanner.next();
-            if (menuOptions.containsKey(option)) {
-                IOperation operation = (IOperation) menuOptions.get(option);
-                operation.execute();
-            }
-            else{
-                IOperation operation = (IOperation) menuOptions.get("5");
-                operation.execute();
-            }
-
+            IOperation operation = menuOptions.getOperation(option);
+            operation.execute();
         }
     }
 }
