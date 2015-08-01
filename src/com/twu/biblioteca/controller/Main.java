f0380cd28cc699dc15444a073eb05103423cf051
Dablu Kumar <dabluk@thoughtworks.com>
2015-08-01 11:12:50 +0530

    [Dablu] List books command added in the main

--- src/com/twu/biblioteca/controller/Main.java
+++ src/com/twu/biblioteca/controller/Main.java
@@ -18,21 +18,24 @@ public class Main {
         Inputs inputs = new Inputs();
         ArrayList<Book> books = inputs.addBooks();
         Library library = new Library(books, new ArrayList<Book>());
-        IView listBooks = new ViewListOfBooks(library);
+        IView viewListOfBooks = new ViewListOfBooks(library);
         IView optionsView = new ViewShowOptions();
         welcomeView.show();
         String  option;
 
+        ListBooks listBooks = new ListBooks(library, viewListOfBooks);
         CheckoutBook checkoutBook = new CheckoutBook(library);
         HashMap menuOptions = new HashMap();
-        menuOptions.put("2",checkoutBook);
+        menuOptions.put("1", listBooks);
+        menuOptions.put("2", checkoutBook);
         while (true){
             optionsView.show();
             System.out.println("Enter Your Choice");
             option = scanner.next();
-            Menu menu = new Menu(library, listBooks);
+//            Menu menu = new Menu(library, listBooks);
             IOperation operation = (IOperation) menuOptions.get(option);
             operation.execute();
+
 //            menu.executeCommand(option);
         }
     }
