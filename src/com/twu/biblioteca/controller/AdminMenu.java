699028cde6781eef6b56bcd4dda54f855062d638
Dablu Kumar <dabluk@thoughtworks.com>
2015-08-06 17:00:00 +0530

    [Dablu] Admin menu modified and added to the application

--- src/com/twu/biblioteca/controller/AdminMenu.java
+++ src/com/twu/biblioteca/controller/AdminMenu.java
@@ -1,4 +1,32 @@
 package com.twu.biblioteca.controller;
 
+import com.twu.biblioteca.model.Library;
+import com.twu.biblioteca.view.IView;
+
+import java.util.Scanner;
+
 public class AdminMenu {
+    private Library library;
+    private AdminMenuOptions menuOptions;
+    private IView view;
+
+
+    public AdminMenu(Library library, AdminMenuOptions adminMenuOptions, IView viewShowAdminMenu) {
+
+        this.library = library;
+        this.menuOptions = adminMenuOptions;
+        view = viewShowAdminMenu;
+    }
+
+    public void show(){
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            view.show();
+            System.out.println("Enter your choice");
+            String option = scanner.next();
+            IOperation operation = menuOptions.getOperation(option);
+            if (operation.execute() == 1)
+                break;
+        }
+    }
 }
