c3026a400db47855b4a3fc304b34f2e27c59ad6f
Dablu Kumar <dabluk@thoughtworks.com>
2015-08-06 16:34:50 +0530

    [Dablu] Admin menu modified and menu added for the listing of checkout movies

--- src/com/twu/biblioteca/controller/AdminMenuOptions.java
+++ src/com/twu/biblioteca/controller/AdminMenuOptions.java
@@ -1,4 +1,40 @@
 package com.twu.biblioteca.controller;
 
+import com.twu.biblioteca.model.Library;
+import com.twu.biblioteca.model.User;
+import com.twu.biblioteca.view.IView;
+import com.twu.biblioteca.view.ViewInvalidMessage;
+import com.twu.biblioteca.view.ViewListCheckoutBooks;
+import com.twu.biblioteca.view.ViewListCheckoutMovies;
+
+import java.util.HashMap;
+
 public class AdminMenuOptions {
+    private HashMap menu;
+    private Library library;
+    private User user;
+
+    public AdminMenuOptions(Library library, User user) {
+        this.library = library;
+        this.user = user;
+        menu = new HashMap();
+        IView viewListCheckoutBooks = new ViewListCheckoutBooks(library);
+        IView viewListCheckoutMovies = new ViewListCheckoutMovies(library);
+        ListCheckoutBooks listCheckoutBooks = new ListCheckoutBooks(library,viewListCheckoutBooks);
+        ListCheckoutMovies listCheckoutMovies = new ListCheckoutMovies(library, viewListCheckoutMovies);
+        Exit exit = new Exit(library);
+        InvalidMessage invalidMessage = new InvalidMessage(new ViewInvalidMessage());
+        menu.put("1", listCheckoutBooks);
+        menu.put("2", listCheckoutMovies);
+        menu.put("4", exit);
+        menu.put("9", invalidMessage);
+
+    }
+
+    public IOperation getOperation(String input){
+
+        if (menu.containsKey(input))
+            return (IOperation) menu.get(input);
+        return (IOperation) menu.get("9");
+    }
 }
